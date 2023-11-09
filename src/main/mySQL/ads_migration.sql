15250c562d87ef431c10a86e43a6424f5cbff5f3
Gerald Walker <gerald.e.walker05@gmail.com>
2023-11-09 16:43:05 -0600

    done with db

--- src/main/mySQL/ads_migration.sql
+++ src/main/mySQL/ads_migration.sql
@@ -6,12 +6,17 @@ CREATE TABLE ads
 (
     id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user        INT UNSIGNED NOT NULL,
-    player_name VARCHAR(50)  NOT NULL,
+    player      VARCHAR(50)  NOT NULL,
+    player_team INT UNSIGNED not null,
+    player_position INT UNSIGNED not null,
     number      int UNSIGNED not null,
     price       int unsigned not null,
-    date_posted date         not null,
     PRIMARY KEY (id),
-    FOREIGN KEY (user) references users (id)
+    FOREIGN KEY (user) references users (id),
+    FOREIGN KEY (player_position) references position (id),
+    FOREIGN KEY (player_team) references teams (id)
 );
+
+
 
 
