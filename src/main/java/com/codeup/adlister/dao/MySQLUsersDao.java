2116000b9e12242018d6fd9ee9049ceb727d2464
William Clayton <william.d.clayton1@gmail.com>
2023-11-13 12:30:21 -0600

    -wc

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,13 +1,14 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
+
 import com.codeup.adlister.util.Config;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
 
 public class MySQLUsersDao implements Users {
-    private Connection connection;
+    private final Connection connection;
 
     public MySQLUsersDao(Config config) {
         try {
