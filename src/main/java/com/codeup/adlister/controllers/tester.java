ad12d95649f0229e413865123cc8b8c0ada01025
Gerald Walker <gerald.e.walker05@gmail.com>
2023-11-13 11:36:07 -0600

    making tester pages

---
+++ src/main/java/com/codeup/adlister/controllers/tester.java
@@ -0,0 +1,9 @@
+package com.codeup.adlister.controllers;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/tester")
+public class tester extends HttpServlet {
+}
---
+++ src/main/webapp/WEB-INF/tester.jsp
@@ -0,0 +1,12 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+
+<h1>Hello world this si the test page</h1>
+
+</body>
+</html>
